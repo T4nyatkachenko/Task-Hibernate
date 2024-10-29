0542db1260df3750af2f305ab96c71bbca23a624
T4nyatkachenko <t4nyatkachenko@yandex.ru>
2024-10-29 21:42:49 +0300

    "Выполнено задание 1.1.3"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,40 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        // Создание таблицы User(ов)
+
+        userService.createUsersTable();
+
+
+       /* Добавление 4 User(ов) в таблицу с данным на свой выбор.
+         После каждого добавления должен быть вывод в консоль
+          (User с именем — name добавлен в базу данных)
+        */
+
+        userService.saveUser("Анна", "Аношина", (byte) 27);
+        userService.saveUser("Иван", "Иванов", (byte) 39);
+        userService.saveUser("Ольга", "Ольгиенко", (byte) 40);
+        userService.saveUser("Петр", "Петров", (byte) 35);
+
+
+
+        // Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
+        userService.getAllUsers();
+
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+
+        // Удаление таблицы
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static Connection conn = null;
+    private static Util instance = null;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Util() {
+        try {
+            conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if (!conn.isClosed()) {
+                System.out.println("Соединение с БД установлено!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Не удалось загрузить класс драйвера!");
+        }
+    }
+
+
+    public static Util getInstance() {
+        if (null == instance) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
